36a90033c20a298634b14cf37ee190f49b29e094
Jirilmon George <jirilmongeorge@gmail.com>
2020-07-03 14:34:26 +0530

    Update udac_example_dag.py

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -2,11 +2,12 @@ from datetime import datetime, timedelta
 import os
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
+from airflow.operators import  PostgresOperator
+from operators import (StageToRedshiftOperator, LoadFactOperator,
                                 LoadDimensionOperator, DataQualityOperator)
 from helpers import SqlQueries
 
-AWS_KEY = os.environ.get('AWS_KEY')
+AWS_KEY = os.environ.get('AWS_KEY') 
 AWS_SECRET = os.environ.get('AWS_SECRET')
 
 default_args = {
